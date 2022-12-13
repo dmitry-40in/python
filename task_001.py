db97cec3878b5b1f0e85dc79c658a0c6cdb87559
Dmitry <dmitry_40in@mail.ru>
2022-12-14 01:28:05 +0300

    task_001 - all done

--- task_001.py
+++ task_001.py
@@ -1,2 +1,9 @@
 # Напишите программу, которая принимает на вход два числа и проверяет, является ли одно число квадратом другого?
 
+number_one = int(input('Введите первое число: '))
+number_two = int(input('Введите второе число: '))
+
+if number_one == number_two ** 2 or number_two == number_one ** 2:
+    print('Да')
+else:
+    print('Нет')
