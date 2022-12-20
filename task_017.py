e6003e124d2b865ee6ab8e591c96491cd4b7669c
Dmitry <dmitry_40in@mail.ru>
2022-12-20 20:34:13 +0300

    h/w №2 done

---
+++ task_017.py
@@ -0,0 +1,19 @@
+# Задайте список из N элементов, заполненных числами из промежутка [-N, N]. 
+# Найдите произведение элементов на указанных позициях. Позиции хранятся в файле file.txt в одной строке одно число.
+
+import random
+
+number_n = int(input('Введите число N: '))
+list = []
+result = 1
+
+for i in range(number_n):
+    list.append(random.randint(-number_n, number_n))
+print(list)
+
+file = open("file.txt", "r")
+
+for i in file:
+    result *= list[int(i)]
+
+print(result)
---
+++ task_018.py
@@ -0,0 +1,10 @@
+# Реализуйте алгоритм перемешивания списка.
+
+import random
+
+list = [1, 2, 3, 4, 5]
+print(list)
+
+random.shuffle(list)
+
+print(list)
